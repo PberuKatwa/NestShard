e3672fac00c2d1262fbd00aeb631b7788e27fa0a
pberu8531 <pberu8531@gmail.com>
2026-01-08 19:17:01 +0300

    S3 garage setup v6:setup garage service imports

--- src/services/garage.service.ts
+++ src/services/garage.service.ts
@@ -3,3 +3,18 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { S3_CLIENT } from "src/storage/garage.storage";
 import { S3Client } from "@aws-sdk/client-s3";
 import { randomUUID } from "crypto";
+
+@Injectable()
+export class GarageService{
+
+    constructor( @Inject(S3_CLIENT) private readonly s3:S3Client ){}
+
+    async uploadFile(){
+        try{
+
+        }catch(error){
+            throw error;
+        }
+    }
+
+}
