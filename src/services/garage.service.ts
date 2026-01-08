163273448c18ed9c681f3b33ff6c88d3d0d1502c
pberu8531 <pberu8531@gmail.com>
2026-01-08 19:03:51 +0300

    S3 garage setup v5:setup garage service imports

--- src/services/garage.service.ts
+++ src/services/garage.service.ts
@@ -1,1 +1,5 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3_CLIENT } from "src/storage/garage.storage";
+import { S3Client } from "@aws-sdk/client-s3";
+import { randomUUID } from "crypto";
