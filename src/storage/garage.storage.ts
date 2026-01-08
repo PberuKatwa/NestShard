219570a684643cc7ccc1140f5695a0cedbdb7429
pberu8531 <pberu8531@gmail.com>
2026-01-08 18:43:28 +0300

    S3 garage setup v3:setup DI module boundary

--- src/storage/garage.storage.ts
+++ src/storage/garage.storage.ts
@@ -2,9 +2,9 @@ import { S3Client,S3ClientConfig } from "@aws-sdk/client-s3";
 import { ConfigService } from "@nestjs/config";
 import { Provider } from "@nestjs/common";
 
-const S3_CLIENT = 'S3_CLIENT';
+export const S3_CLIENT = 'S3_CLIENT';
 
-const s3CLientProvider:Provider = {
+export const s3CLientProvider:Provider = {
     provide:S3Client,
     useFactory: function (configService:ConfigService){
         return new S3Client({
@@ -19,7 +19,3 @@ const s3CLientProvider:Provider = {
     },
     inject:[ConfigService]
 }
-
-
-
-export default { S3_CLIENT, s3CLientProvider};
