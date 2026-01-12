a7f0ac2a3dccd0cbe9734fac63954e0a89878169
pberu8531 <pberu8531@gmail.com>
2026-01-12 18:37:52 +0300

    making logger global v2:global logger

--- src/modules/files/files.module.ts
+++ src/modules/files/files.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { GarageService } from "src/services/garage.service";
-import { AppLoggerModule } from "src/logger/logger.module";
 
 @Module({
   providers:[GarageService]
