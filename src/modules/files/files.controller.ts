766662ae623b8bb335cd4efc925cc0f094fb1c7c
pberu8531 <pberu8531@gmail.com>
2026-01-12 18:51:17 +0300

    List S3 files v7:injected app logger

--- src/modules/files/files.controller.ts
+++ src/modules/files/files.controller.ts
@@ -1,1 +1,18 @@
-import { Get, Controller } from "@nestjs/common";
+import { Get, Controller,Res, Inject } from "@nestjs/common";
+import { GarageService } from "src/services/garage.service";
+import type { Response } from "express";
+import { APP_LOGGER } from "src/logger/logger.provider";
+import type { AppLogger } from "src/logger/winston.logger";
+
+@Controller('files')
+export class FilesController{
+
+  private readonly garageService: GarageService;
+  @Inject(APP_LOGGER) private readonly logger: AppLogger;
+
+  constructor(
+  ) {
+
+  }
+
+}
