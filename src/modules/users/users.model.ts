7189f264b452f8e5fa5b6531ef60bb853cca384d
Pberu <pberu8531@gmail.com>
2026-01-19 19:42:10 +0300

    users setup v9:created pool

--- src/modules/users/users.model.ts
+++ src/modules/users/users.model.ts
@@ -1,15 +1,19 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { Pool } from "pg";
 import { PostgresConfig } from "src/databases/postgres.config";
 import { APP_LOGGER } from "src/logger/logger.provider";
 import type { AppLogger } from "src/logger/winston.logger";
 
 @Injectable()
 export class UsersModel{
+  private readonly pool: Pool | null;
 
   constructor(
     @Inject(APP_LOGGER) private readonly logger: AppLogger,
     private readonly pgConfig:PostgresConfig
-  ) { }
+  ) {
+    this.pool = PostgresConfig.getPool()
+  }
 
   async createTable():Promise<string> {
     try {
@@ -21,10 +25,14 @@ export class UsersModel{
           id SERIAL PRIMARY KEY,
           first_name TEXT NOT NULL,
           last_name TEXT NOT NULL,
-          email TEXT,
+          email TEXT NOT NULL,
           password VARCHAR NOT NULL,
+          created_at TIMESTAMPTZ,
+          updated_at TIMESTAMPTZ
         );
       `
+
+      await this.pgConfig.connect
 
     } catch (error) {
       throw error;
