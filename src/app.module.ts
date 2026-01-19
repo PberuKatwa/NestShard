e00c0ef26c537175fb7bece3bd03d0c388c005fd
Pberu <pberu8531@gmail.com>
2026-01-19 18:32:10 +0300

    postgres setup v12:imported postgres module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { s3Config,postgresEnv,globalConfig } from './config';
 import { AppLoggerModule } from './logger/logger.module';
 import { GarageModule } from './modules/garage/garage.module';
 import { FilesModule } from './modules/files/files.module';
+import { PostgresModule } from './databases/postgres.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load:[s3Config,postgresEnv, globalConfig], isGlobal:true }),
     AppLoggerModule,
     GarageModule,
-    FilesModule
+    FilesModule,
+    PostgresModule
   ]
 })
 export class AppModule {}
