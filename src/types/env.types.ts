5374e96cc489b7fbb0886b9cd053f286072062c7
Pberu <pberu8531@gmail.com>
2026-01-19 16:53:54 +0300

    Env global revamp v4:setup s3 config

--- src/types/env.types.ts
+++ src/types/env.types.ts
@@ -8,6 +8,14 @@ export interface EnvConfig{
   pgDatabase: string
 }
 
+export interface s3Config{
+  s3Endpoint: string,
+  s3Region:string,
+  s3AccessKey:string,
+  s3SecretKey:string,
+  s3Bucket:string
+}
+
 export type SuffixChecker = (value:string,suffix:string) => boolean;
 export type GlobalEnvironmentChecker = () => string;
 export type GetEnv = (globalEnv:GlobalEnvironmentChecker, key: string) => string;
