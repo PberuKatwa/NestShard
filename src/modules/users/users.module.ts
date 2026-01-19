026a38437ed176c93a389caaeddd2660e2b66898
Pberu <pberu8531@gmail.com>
2026-01-19 19:59:43 +0300

    users setup v13:imported provider to module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { UsersModel } from "./users.model";
 
 @Module({
-
+  providers: [ UsersModel ],
+  exports:[ UsersModel ]
 })
 
 export class UserModule {}
