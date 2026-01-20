07b65de47283a265a384e780c7f381b29802a732
Pberu <pberu8531@gmail.com>
2026-01-20 18:41:43 +0300

    User registration v5:added unique email constraint

---
+++ migrations/1768923554680_email-unique-constraint.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addConstraint('users', 'unique_email_contraint', {
+    unique:'email'
+  })
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropConstraint('users', 'unique_email_contraint')
+};
