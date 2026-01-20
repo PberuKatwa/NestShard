95d453ba9383576bc63916c0c29919ea212f8b7c
Pberu <pberu8531@gmail.com>
2026-01-20 18:05:10 +0300

    Auth setup v5:draft create user method

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Inject, Req, Res } from "@nestjs/common";
+import { Controller, Inject, Post, Req, Res } from "@nestjs/common";
 import type { Request, Response } from "express";
 import type { AppLogger } from "src/logger/winston.logger";
 import { APP_LOGGER } from "src/logger/logger.provider";
 import { UsersModel } from "../users/users.model";
+import type { ApiResponse } from "src/types/api.types";
 
 @Controller('auth')
 export class AuthController{
@@ -12,11 +13,33 @@ export class AuthController{
     private readonly user:UsersModel
   ) { }
 
-  async createUser( @Req() req:Request, @Res() res:Response ) {
+  @Post('register')
+  async createUser( @Req() req:Request, @Res() res:Response ):Promise<Response> {
     try {
 
-    } catch(error) {
+      const { firstName, lastName, email, password } = req.body;
+
+      const user = await this.user.createUser(firstName, lastName, email, password)
+
+      const response: ApiResponse = {
+        success: true,
+        message: `Successfully created user with email ${email}`,
+        data:user
+      }
+
+      return res.status(200).json(response)
+
+    } catch (error) {
+
       this.logger.error(`Error in creating user`, error)
+
+      const response: ApiResponse = {
+        success: true,
+        message: `${error.message}`,
+      }
+
+      return res.status(500).json(response)
+
     }
   }
 
