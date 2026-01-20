0c0e72e7a01a1214adcd204c507726e5926a1820
Pberu <pberu8531@gmail.com>
2026-01-20 17:54:15 +0300

    Auth setup v4:setup method to create user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Inject } from "@nestjs/common";
+import { Controller, Inject, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
 import type { AppLogger } from "src/logger/winston.logger";
 import { APP_LOGGER } from "src/logger/logger.provider";
 import { UsersModel } from "../users/users.model";
@@ -10,5 +11,13 @@ export class AuthController{
     @Inject(APP_LOGGER) private readonly logger: AppLogger,
     private readonly user:UsersModel
   ) { }
+
+  async createUser( @Req() req:Request, @Res() res:Response ) {
+    try {
+
+    } catch(error) {
+      this.logger.error(`Error in creating user`, error)
+    }
+  }
 
 }
