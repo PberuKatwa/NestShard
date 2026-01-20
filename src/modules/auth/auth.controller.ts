09e844dac0c58329141a27621c3060bc674cf50c
Pberu <pberu8531@gmail.com>
2026-01-20 17:49:53 +0300

    Auth setup v3:setup constructor in Auth Controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Inject } from "@nestjs/common";
+import type { AppLogger } from "src/logger/winston.logger";
+import { APP_LOGGER } from "src/logger/logger.provider";
+import { UsersModel } from "../users/users.model";
 
-@Controller()
+@Controller('auth')
 export class AuthController{
+
+  constructor(
+    @Inject(APP_LOGGER) private readonly logger: AppLogger,
+    private readonly user:UsersModel
+  ) { }
 
 }
