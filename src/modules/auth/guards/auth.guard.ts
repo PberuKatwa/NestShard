6ee4194a050bcc396f34a4a794914b7485b2f503
Pberu <pberu8531@gmail.com>
2026-01-21 18:28:53 +0300

    Auth guard setup v3:extracted authorization header

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -1,6 +1,24 @@
-import { CanActivate, Injectable, ExecutionContext } from "@nestjs/common";
+import { CanActivate, Injectable, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { Observable } from "rxjs";
 
 @Injectable()
-export class AuthGuard{
+export class AuthGuard implements CanActivate{
+
+  async canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+
+    try {
+      const request = context.switchToHttp().getRequest();
+      const authorization:string = request.headers.authorization;
+
+      if (!authorization || authorization.trim() === '') {
+
+      }
+
+      const token = authorization?.split(' ')[1];
+    } catch (error) {
+      throw error;
+    }
+
+  }
 
 }
