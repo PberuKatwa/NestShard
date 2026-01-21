35a3427abc1e58b4b0866001849f4495f6b5c8af
Pberu <pberu8531@gmail.com>
2026-01-21 19:07:47 +0300

    Auth guard setup v5:setup profile route to test

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post, Req, Res, Get, HttpException, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Inject, Post, Req, Res, Get, HttpException, HttpStatus, UseGuards } from "@nestjs/common";
 import type { Request, Response } from "express";
 import type { AppLogger } from "src/logger/winston.logger";
 import { APP_LOGGER } from "src/logger/logger.provider";
@@ -6,6 +6,7 @@ import { UsersModel } from "../users/users.model";
 import type { ApiResponse } from "src/types/api.types";
 import { RegisterUserDto } from "./dto/register-user.dto";
 import { LoginUserDto } from "./dto/login-user.dto";
+import { AuthGuard } from "./guards/auth.guard";
 
 @Controller('auth')
 export class AuthController{
@@ -62,7 +63,7 @@ export class AuthController{
       return response
 
     } catch (error) {
-      this.logger.error(`Error in loggin in user`, error)
+      this.logger.error(`Error in logging in user`, error)
       const response: ApiResponse = {
         success: false,
         message: `${error.message}`,
@@ -70,6 +71,30 @@ export class AuthController{
       throw new HttpException(response,HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  async getProfile(): Promise<ApiResponse> {
+    try {
+
+      const response: ApiResponse = {
+        success: false,
+        message:"Successfully obtained user profile"
+      }
+
+      return response
+
+    } catch (error) {
+
+      this.logger.error(`Error in getting user profile`, error)
+      const response: ApiResponse = {
+        success: false,
+        message: `${error.message}`,
+      }
+      throw new HttpException(response, HttpStatus.INTERNAL_SERVER_ERROR)
+
+    }
   }
 
 }
