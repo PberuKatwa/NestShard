3a22b64be247dc57d6d9319be10203cefa244f4f
Pberu <pberu8531@gmail.com>
2026-01-21 20:51:51 +0300

    User decorator v2:draft file

--- src/modules/users/decorators/user.decorator.ts
+++ src/modules/users/decorators/user.decorator.ts
@@ -1,1 +1,13 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { DecodedUser } from "src/types/users.types";
+
+export const currentUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+
+    console.log("userrrrr", user)
+
+    return data ? user?.[data] : user;
+  },
+)
