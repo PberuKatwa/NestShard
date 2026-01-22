86456407a4f10056481fa123d028b8f9ee2ed306
Pberu <pberu8531@gmail.com>
2026-01-22 19:50:26 +0300

    Properties module setup v6:created class and instantiated variables

--- src/modules/properties/properties.model.ts
+++ src/modules/properties/properties.model.ts
@@ -3,3 +3,16 @@ import { Pool } from "pg";
 import { PostgresConfig } from "src/databases/postgres.config";
 import { APP_LOGGER } from "src/logger/logger.provider";
 import type { AppLogger } from "src/logger/winston.logger";
+
+@Injectable()
+export class PropertiesModel {
+  private readonly pool: Pool;
+
+  constructor(
+    @Inject(APP_LOGGER) private readonly logger: AppLogger,
+    private readonly pgConfig:PostgresConfig
+  ) {
+    this.pool =
+  };
+
+}
