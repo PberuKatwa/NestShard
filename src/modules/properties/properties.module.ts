34b93d8f90d2337a82a141d465dadd416b3ef796
Pberu <pberu8531@gmail.com>
2026-01-22 20:57:40 +0300

    Properties module setup v13:imported the module

--- src/modules/properties/properties.module.ts
+++ src/modules/properties/properties.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UsersModel } from "../users/users.model";
+import { PropertiesModel } from "./properties.model";
 
 @Module({
-  imports:[UsersModel]
+  imports: [UsersModel],
+  providers: [PropertiesModel],
+  exports:[PropertiesModel]
 })
 
 export class PropertiesModule{}
