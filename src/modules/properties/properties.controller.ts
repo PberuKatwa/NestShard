0233e5b69be7505d3fe7964297c15945b975835e
Pberu <pberu8531@gmail.com>
2026-01-22 21:51:00 +0300

    create properties api v6:setup types

--- src/modules/properties/properties.controller.ts
+++ src/modules/properties/properties.controller.ts
@@ -1,4 +1,9 @@
-import { Body, Controller, Inject, Post, Req, Res, Get, HttpException, HttpStatus, UseGuards } from "@nestjs/common";
+import {
+  Body, Controller, Inject, Post, Req,
+  Res, Get, HttpException, HttpStatus,
+  UseGuards, UseInterceptors, UploadedFile,
+  ParseFilePipe, MaxFileSizeValidator, FileTypeValidator
+} from "@nestjs/common";
 import type { Request, Response } from "express";
 import type { AppLogger } from "src/logger/winston.logger";
 import { APP_LOGGER } from "src/logger/logger.provider";
@@ -6,6 +11,7 @@ import { UsersModel } from "../users/users.model";
 import type { ApiResponse } from "src/types/api.types";
 import { AuthGuard } from "../auth/guards/auth.guard";
 import { PropertiesModel } from "./properties.model";
+import { FileInterceptor } from "@nestjs/platform-express";
 
 
 @Controller('properties')
@@ -17,8 +23,22 @@ export class PropertyController{
   ) { }
 
   @Post('')
-  async createProperty() {
+  @UseInterceptors( FileInterceptor('image') )
+  async createProperty(
+    @Body() body:any,
+    @UploadedFile(
+        new ParseFilePipe({
+          validators: [
+            new MaxFileSizeValidator({ maxSize: 5242880 }),
+            new FileTypeValidator({ fileType: '.(png|jpeg|jpg|webp)' }),
+          ],
+        }),
+      )
+      file: Express.Multer.File,
+  ) {
     try {
+
+
 
     } catch (error) {
       this.logger.error(`Error in creating property`, error)
