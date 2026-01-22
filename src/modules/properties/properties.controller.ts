8a4b68802cbd6aa4c191f08de583c83f423d46df
Pberu <pberu8531@gmail.com>
2026-01-22 21:41:54 +0300

    create properties api v5:setup api

--- src/modules/properties/properties.controller.ts
+++ src/modules/properties/properties.controller.ts
@@ -5,6 +5,32 @@ import { APP_LOGGER } from "src/logger/logger.provider";
 import { UsersModel } from "../users/users.model";
 import type { ApiResponse } from "src/types/api.types";
 import { AuthGuard } from "../auth/guards/auth.guard";
+import { PropertiesModel } from "./properties.model";
 
 
 @Controller('properties')
+export class PropertyController{
+
+  constructor(
+    @Inject(APP_LOGGER) private readonly logger: AppLogger,
+    private readonly properties:PropertiesModel
+  ) { }
+
+  @Post('')
+  async createProperty() {
+    try {
+
+    } catch (error) {
+      this.logger.error(`Error in creating property`, error)
+
+      const response: ApiResponse = {
+        success: false,
+        message: `${error.message}`,
+      }
+
+      throw new HttpException(response,HttpStatus.INTERNAL_SERVER_ERROR)
+
+    }
+  }
+
+}
