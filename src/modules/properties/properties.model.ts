fd0930e93e9353047dc901b851c2b1553c760560
Pberu <pberu8531@gmail.com>
2026-01-22 19:47:14 +0300

    Properties module setup v5:created model

--- src/modules/properties/properties.model.ts
+++ src/modules/properties/properties.model.ts
@@ -1,1 +1,5 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { Pool } from "pg";
+import { PostgresConfig } from "src/databases/postgres.config";
+import { APP_LOGGER } from "src/logger/logger.provider";
+import type { AppLogger } from "src/logger/winston.logger";
