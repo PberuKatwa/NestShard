05a86612e7049116864afe47f479522a82fb813a
Pberu <pberu8531@gmail.com>
2026-01-24 13:54:01 +0300

    create blog api v4:returned error

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Inject, Post, Req, Res } from "@nestjs/common";
 import { APP_LOGGER } from "src/logger/logger.provider";
 import type { AppLogger } from "src/logger/winston.logger";
+import type { ApiResponse } from "src/types/api.types";
 
 @Controller('blogs')
 export class BlogController{
@@ -11,11 +12,20 @@ export class BlogController{
   ) { }
 
   @Post('')
-  async createPost( @Req() req:Request, @Res() res:Response ) {
+  async createPost( @Req() req:Request, @Res() res:Response ):Promise<ApiResponse> {
     try {
 
     } catch (error) {
-      this.logger.error(``)
+
+      this.logger.error(`error in creating blog post`, error)
+
+      const response: ApiResponse = {
+        success: false,
+        message:`${error}`
+      }
+
+      return response;
+
     }
   }
 
