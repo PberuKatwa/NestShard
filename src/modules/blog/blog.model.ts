dfe36af3e44ae6671b9aeb3172786de9f6ecaad8
Pberu <pberu8531@gmail.com>
2026-01-24 13:02:07 +0300

    Blog Model setup v1:setup query for table construction

--- src/modules/blog/blog.model.ts
+++ src/modules/blog/blog.model.ts
@@ -1,17 +1,34 @@
 import { Injectable,Inject } from "@nestjs/common";
+import { Pool } from "pg";
 import { APP_LOGGER } from "src/logger/logger.provider";
 import type { AppLogger } from "src/logger/winston.logger";
 
 @Injectable()
 export class BlogModel{
 
+  private readonly pgPool:Pool;
   constructor(
-    @Inject(APP_LOGGER) private readonly logger:AppLogger
-
+    @Inject(APP_LOGGER) private readonly logger:AppLogger,
+    pgPool:Pool
   ) {
+    this.pgPool = pgPool;
   }
 
   async createTable() {
+
+    this.logger.warn(`Attempting to create blogs table`);
+    const query = `
+
+      CREATE TABLE blogs IF NOT EXISTS(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(240) NOT NULL,
+        author_id INTEGER NOT NULL,
+        slug VARCHAR(240),
+        created_at TIMESTAMPZ NOT NULL,
+        updated_at TIMESTAMPZ NOT NULL
+      )
+
+    `
 
   }
 
