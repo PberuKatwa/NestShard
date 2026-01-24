34943675b2df333be2e60fc05d43058e7de08045
Pberu <pberu8531@gmail.com>
2026-01-24 13:46:03 +0300

    create blog api v2:setup api class constructor

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Inject } from "@nestjs/common";
+import { APP_LOGGER } from "src/logger/logger.provider";
+import type { AppLogger } from "src/logger/winston.logger";
 
 @Controller('blogs')
 export class BlogController{
 
+  constructor(
+    @Inject(APP_LOGGER) private readonly logger:AppLogger
 
+  ) {}
 
 }
