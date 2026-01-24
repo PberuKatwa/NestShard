32c2535f27ae1588257cbdad5bcce4d2ae67ce0a
Pberu <pberu8531@gmail.com>
2026-01-24 14:05:46 +0300

    create blog api v7:returned api response

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { BlogModel } from "./blog.model";
+import { BlogController } from "./blog.controller";
 
 @Module({
   providers: [BlogModel],
+  controllers:[BlogController],
   exports:[BlogModel]
 })
 
