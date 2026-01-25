87555edc2cc3cf84f03783d559cc675bb6db15da
Pberu <pberu8531@gmail.com>
2026-01-25 21:08:23 +0300

    postgres globals setup v4:added query

--- src/databases/postgres.globals.ts
+++ src/databases/postgres.globals.ts
@@ -9,6 +9,24 @@ export class PostgresGlobals{
   async initializeTypes() {
     try {
 
+      const query = `
+        DO $$
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'row_status') THEN
+                CREATE TYPE property_status AS ENUM ('active', 'trash', 'pending' );
+            END IF;
+        END
+        $$;
+
+        DO $$
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
+                CREATE TYPE property_status AS ENUM ('super_admin', 'admin', 'ordinary' );
+            END IF;
+        END
+        $$;
+      `;
+
     } catch(error) {
       throw error;
     }
