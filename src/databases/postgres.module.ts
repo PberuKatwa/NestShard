2976d531c171d6162957720d5832d40960627f8e
Pberu <pberu8531@gmail.com>
2026-01-25 21:34:13 +0300

    postgres globals setup v9:initialized trigger function

--- src/databases/postgres.module.ts
+++ src/databases/postgres.module.ts
@@ -7,11 +7,12 @@ import { PropertiesModule } from "src/modules/properties/properties.module";
 import { PropertiesModel } from "src/modules/properties/properties.model";
 import { BlogModel } from "src/modules/blog/blog.model";
 import { BlogModule } from "src/modules/blog/blog.module";
+import { PostgresGlobals } from "./postgres.globals";
 
 @Global()
 @Module({
   imports: [AppLoggerModule, UsersModule, PropertiesModule, BlogModule],
-  providers:[PostgresConfig],
+  providers:[PostgresConfig,PostgresGlobals],
   exports:[PostgresConfig]
 })
 
@@ -21,12 +22,14 @@ export class PostgresModule implements OnModuleInit {
     private readonly postgresConfig: PostgresConfig,
     private readonly users: UsersModel,
     private readonly properties: PropertiesModel,
-    private readonly blog:BlogModel
+    private readonly blog: BlogModel,
+    private readonly pgGlobals:PostgresGlobals
   ) { };
 
   async onModuleInit() {
 
     const pgPool = await this.postgresConfig.connect()
+    await this.pgGlobals.createTimestampTrigger()
     await this.users.createTable()
     await this.properties.createTable()
     await this.blog.createTable()
