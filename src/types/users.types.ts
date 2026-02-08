05c3005357a50874bdc4ff47771e7b77c92b7d2a
Pberu <pberu8531@gmail.com>
2026-02-08 10:29:57 +0300

    clean up

--- src/types/users.types.ts
+++ src/types/users.types.ts
@@ -1,16 +1,5 @@
 import type { ApiResponse } from "./api.types";
 
-export interface User {
-  id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  fileId: number;
-  access_token?: string;
-  signedUrl?: string;
-}
-
-
 export interface BaseUser {
   first_name: string;
 }
@@ -47,14 +36,6 @@ export interface UserProfile extends BaseUser {
   file_id: number | null;
   file_url: string | null;
   signed_url: string | null;
-}
-
-export interface DecodedUser {
-  userId: string,
-  email: string,
-  userName: string,
-  iat: number,
-  exp:number
 }
 
 export interface UserApiResponse extends ApiResponse<BaseUser> { };
