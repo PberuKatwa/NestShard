677ada7e83f902444b36d9d066384a846dfec7e2
Pberu <pberu8531@gmail.com>
2026-02-09 21:43:58 +0300

    Logger middleware v2:imported logger

--- src/common/middleware/logger/logger.middleware.ts
+++ src/common/middleware/logger/logger.middleware.ts
@@ -1,4 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, Next } from '@nestjs/common';
+import { logger } from 'src/logger/winston.logger';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
