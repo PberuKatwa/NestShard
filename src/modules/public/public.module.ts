8f1a3c8b5a80ab09098da3c3d65ed153321d6235
Pberu <pberu8531@gmail.com>
2026-02-23 16:36:28 +0300

    Public api setup v4:added models

--- src/modules/public/public.module.ts
+++ src/modules/public/public.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PropertiesModule } from "../properties/properties.module";
 import { BlogModule } from "../blog/blog.module";
+import { PropertiesModel } from "../properties/properties.model";
+import { BlogModel } from "../blog/blog.model";
 
 @Module({
-  imports:[PropertiesModule,BlogModule]
+  imports: [PropertiesModule, BlogModule],
+  providers:[PropertiesModel,BlogModel]
 })
 
 export class PublicModule { };
