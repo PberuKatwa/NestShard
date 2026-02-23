2f7025459f5fe96299fb611bcf5724bf6282db12
Pberu <pberu8531@gmail.com>
2026-02-23 16:51:56 +0300

    Public api setup v7:setup error instance

--- src/modules/public/public.controller.ts
+++ src/modules/public/public.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Inject } from "@nestjs/common";
+import { Controller,Inject, Get, Query } from "@nestjs/common";
 import { PropertiesModel } from "../properties/properties.model";
 import { BlogModel } from "../blog/blog.model";
 import { APP_LOGGER } from "src/logger/logger.provider";
@@ -13,6 +13,23 @@ export class PublicController{
     @Inject(APP_LOGGER) private readonly logger:AppLogger
   ) { }
 
+  @Get('properties')
+  async getProperties(
+    @Query('page') page: number,
+    @Query('limit') limit:number
+  ) {
+    try {
 
+    } catch (error: any) {
+
+      let message = "Unknown error";
+      let stack = null;
+
+      this.logger.error(`Error in fetching properties for public api`, {
+        errorMessage: error.message,
+        errorStack:error.stack
+      })
+    }
+  }
 
 }
