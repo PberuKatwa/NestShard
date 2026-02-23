f8a417c43136db81a23de87be5dc6cac2aaaa44e
Pberu <pberu8531@gmail.com>
2026-02-23 16:43:08 +0300

    Public api setup v6:setup constructor

--- src/modules/public/public.controller.ts
+++ src/modules/public/public.controller.ts
@@ -1,6 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Controller,Inject } from "@nestjs/common";
+import { PropertiesModel } from "../properties/properties.model";
+import { BlogModel } from "../blog/blog.model";
+import { APP_LOGGER } from "src/logger/logger.provider";
+import type { AppLogger } from "src/logger/winston.logger";
 
 @Controller('public')
 export class PublicController{
+
+  constructor(
+    private readonly properties: PropertiesModel,
+    private readonly blog: BlogModel,
+    @Inject(APP_LOGGER) private readonly logger:AppLogger
+  ) { }
+
+
 
 }
